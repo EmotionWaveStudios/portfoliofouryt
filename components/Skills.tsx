f432f6dd6509f2b78086195565335ec1a385d038
Mzwandile Nkohla <73034171+EmotionWaveStudios@users.noreply.github.com>
2026-05-05 19:50:52 +0200

    Refactor skills inputs in Skills component

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -7,18 +7,6 @@ const Skills = () => {
     <div className="wrapper">
       <Title text="Skills" icon={<GiSkills />} />
       <div className="flex gap-4 flex-wrap">
-        <SkillsInput
-          title="Javascript"
-          link="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-        />
-        <SkillsInput title="Reactjs" link="https://react.dev/" />
-        <SkillsInput title="Nextjs" link="https://nextjs.org/" />
-        <SkillsInput title="Nodejs" link="https://nodejs.org/en" />
-        <SkillsInput
-          title="Typescript"
-          link="https://www.typescriptlang.org/"
-        />
-        <SkillsInput title="MongoDB" link="https://www.mongodb.com/" />
         <SkillsInput title="Tailwindcss" link="https://tailwindcss.com/" />
         <SkillsInput
           title="HTML5"
@@ -28,13 +16,13 @@ const Skills = () => {
           title="CSS3"
           link="https://developer.mozilla.org/en-US/docs/Web/CSS"
         />
-        <SkillsInput title="VS Code" link="https://code.visualstudio.com/" />
-        <SkillsInput title="Git" link="https://git-scm.com/" />
-        <SkillsInput title="Github" link="https://github.com/" />
+        <SkillsInput
+          title="JavaScript"
+          link="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+        />
+        <SkillsInput title="Reactjs" link="https://react.dev/" />
         <SkillsInput title="Figma" link="https://www.figma.com/" />
         <SkillsInput title="Canva" link="https://www.canva.com/" />
-        <SkillsInput title="Vercel" link="https://vercel.com/" />
-        <SkillsInput title="netlify" link="https://www.netlify.com/" />
       </div>
     </div>
   );
